c57dee00005939dc5714922ff6062c58dc862ac5
ksupdev <karoons.updev@gmail.com>
2025-04-06 20:22:41 +0700

    Create Homepage Components & Footer

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
 //sfc
 
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-3xl">
-        Welcome
-      </h1>
-      <Link href='/properties'>Go to Properties</Link>
+      <Hero />
+      <InfoBoxes />
     </div>
   );
 };
