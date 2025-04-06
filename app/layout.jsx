21a150a3fab7287928efc04f90bef9dd474fbfc2
ksupdev <karoons.updev@gmail.com>
2025-04-06 20:59:20 +0700

    Properties Page & PropertyCard Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
 import "@/assets/styles/global.css";
 import Footer from "@/components/Footer";
 
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,8 +1,26 @@
 //sfc
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
 
 const PropertiesPage = () => {
   //http://localhost:3000/properties
-  return <div>PropertiesPage</div>;
+
+  console.log(properties);
+  return (
+    <section className='px-4 py-6'>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesPage;
