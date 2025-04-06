cde4083618e2eec214dd193ddcbeec743707ee22
ksupdev <karoons.updev@gmail.com>
2025-04-06 21:21:41 +0700

    Home Property Listings

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
 //sfc
 
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <Hero />
       <InfoBoxes />
-    </div>
+      <HomeProperties />
+    </>
   );
 };
 
