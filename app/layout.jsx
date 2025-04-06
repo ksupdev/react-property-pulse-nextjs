f605dd1d395265924565294f99c93d4b22c8d5e4
ksupdev <karoons.updev@gmail.com>
2025-04-06 10:24:40 +0700

    Simple Layout & Homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,13 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import AuthProvider from '@/components/AuthProvider';
-import { GlobalProvider } from '@/context/GlobalContext';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '@/assets/styles/globals.css';
-import 'photoswipe/dist/photoswipe.css';
+import '@/assets/styles/global.css';
 
-export const metadata = {
-  title: 'PropertyPulse',
-  description: 'Find The Perfect Rental Property',
-  keywords: 'rental, property, real estate',
-};
-
+//sfc
 const MainLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <GlobalProvider>
-        <html lang='en'>
-          <body>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
-            <ToastContainer />
-          </body>
-        </html>
-      </GlobalProvider>
-    </AuthProvider>
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
   );
 };
 
---
+++ app/page.jsx
@@ -0,0 +1,6 @@
+//sfc
+const HomePage = () => {
+  return <div className="text-2xl">Home Page</div>;
+};
+
+export default HomePage;
