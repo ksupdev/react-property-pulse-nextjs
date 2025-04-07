e6293571dda6512775569a94a51120915531118a
ksupdev <karoons.updev@gmail.com>
2025-04-07 15:59:38 +0700

    Add Property Form and setup Server Action

---
+++ app/actions/addProperty.js
@@ -0,0 +1,40 @@
+'use server';
+
+async function addProperty(formData) {
+    // Access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData.getAll('images').filter((image) => image.name !== '')
+        .map((image) => image.name);
+
+    // Create the propertyData object with embedded seller_info
+    const propertyData = {
+        type: formData.get('type'),
+        name: formData.get('name'),
+        description: formData.get('description'),
+        location: {
+            street: formData.get('location.street'),
+            city: formData.get('location.city'),
+            state: formData.get('location.state'),
+            zipcode: formData.get('location.zipcode'),
+        },
+        beds: formData.get('beds'),
+        baths: formData.get('baths'),
+        square_feet: formData.get('square_feet'),
+        amenities,
+        rates: {
+            weekly: formData.get('rates.weekly'),
+            monthly: formData.get('rates.monthly'),
+            nightly: formData.get('rates.nightly.'),
+        },
+        seller_info: {
+            name: formData.get('seller_info.name'),
+            email: formData.get('seller_info.email'),
+            phone: formData.get('seller_info.phone'),
+        },
+        images,
+    };
+
+    console.log(propertyData);
+}
+
+export default addProperty;
