1437abf9c0373bb5e90fd168fce8b3b8a9f2a7ea
ksupdev <karoons.updev@gmail.com>
2025-04-07 10:42:50 +0700

    config image from external and set profile image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'xxxxx.com',
+                pathname: '**',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
