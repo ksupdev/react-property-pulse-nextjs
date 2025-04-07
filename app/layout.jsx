8f3644463eb9b8e56864c51ea1340f6276591fe4
ksupdev <karoons.updev@gmail.com>
2025-04-07 19:28:52 +0700

    Toast Notifications

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import Navbar from '@/components/Navbar';
 import "@/assets/styles/global.css";
 import AuthProvider from '@/components/AuthProvider';
 import Footer from "@/components/Footer";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   title: "PropertyPulse",
@@ -18,6 +20,7 @@ const MainLayout = ({ children }) => {
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <ToastContainer />
         </body>
       </html>
     </AuthProvider>
