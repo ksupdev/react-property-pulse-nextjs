5eb50f2dc715d050d8ebef89932b8b44fe198f67
ksupdev <karoons.updev@gmail.com>
2025-04-07 18:34:40 +0700

    config layout space

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -11,11 +11,11 @@ const PropertiesPage = async () => {
   // console.log(properties);
   return (
     <section className='px-4 py-6'>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
+      <div className=" max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-start">
         {properties.length === 0 ? (
           <p>No properties found</p>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {properties.map((property) => (
               <PropertyCard key={property._id} property={property} />
             ))}
