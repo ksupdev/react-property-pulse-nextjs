ff67afe75b9d9024446b42d980f3ee98e5108b2c
ksupdev <karoons.updev@gmail.com>
2025-04-08 21:07:29 +0700

    delete message

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -2,14 +2,28 @@
 import { useState } from "react";
 import { toast } from 'react-toastify';
 import markMessageAsRead from "@/app/actions/markMessageAsRead";
+import deleteMessage from "@/app/actions/deleteMessage";
 
 const MessageCard = ({ message }) => {
 
     const [isRead, setIsRead] = useState(message.read);
+    const [isDeleted, setIsDeleted] = useState(false);
+
     const handleReadClick = async () => {
         const read = await markMessageAsRead(message._id);
         setIsRead(read);
-        toast.success(`Mark as ${read ? 'read' : 'new'}`);
+        toast.success(`Mark As ${read ? 'Read' : 'New'}`);
+    }
+
+    const handleDeleteClick = async () => {
+        await deleteMessage(message._id);
+        setIsDeleted(true);
+        toast.success('Message Deleted');
+
+    }
+
+    if(isDeleted){
+        return <p>Deleted Message</p>
     }
 
     return <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
@@ -51,7 +65,7 @@ const MessageCard = ({ message }) => {
             Mark as Read
             {isRead ? 'Mark As New' : 'Mark As Read'}
         </button>
-        <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+        <button onClick={handleDeleteClick} className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
             Delete
         </button>
     </div >;
